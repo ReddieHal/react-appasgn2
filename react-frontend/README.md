ab9d0e8defd44b661ad1af4f71f22f7c5f8ff463
Gabriel Rodriguez <grodri87@calpoly.edu>
2023-10-01 19:37:34 -0700

    oops

---
+++ react-frontend/README.md
@@ -0,0 +1,3 @@
+# react-appasgn2
+
+Just a simple git setup lab
--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -16,31 +16,38 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-          <td>
-			<button onClick={() => 
-				props.removeCharacter(index)}>
-				Delete
-			</button>
-	    </td>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+                <button onClick={() => 
+                    props.removeCharacter(index)}>
+                    Delete
+                </button>
+        </td>
         </tr>
-      );
+    
+      );    
      }
     );
+    return (
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  
     
   }
   
   
 
-  function Table(props) {
+  function Table (props) {
     return (
       <table>
         <TableHeader />
         <TableBody characterData={props.characterData} 
-	            removeCharacter={props.removeCharacter} />
+            removeCharacter={props.removeCharacter} />
       </table>
     );
-}
+  }
 
 export default Table;
